41ae3615e3bb7c46c4f08df8b752da8050479cd9
Elijah Tarr <elijahotarr@gmail.com>
2016-02-24 18:12:47 -0800

    Test

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,6 +11,7 @@ public class CameraController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         offset = transform.position;
+        polyparts = PlayerController.polyparts;
 	}
 	
 	// Update is called once per frame
